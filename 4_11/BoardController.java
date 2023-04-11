4822f620f6c607bd5b39d39f4b0d20813912852b
gahhyun <https://github.com/gahhyun>
2023-04-11 16:04:31 +0900

    4/11 spring 로그인유무에 따른 페이지 이동

---
+++ 4_11/BoardController.java
@@ -0,0 +1,30 @@
+package com.earth.downtown;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/list")
+	public String list(HttpServletRequest request) {
+		
+		if(!loginCheck(request))		//로그인 안했으면 로그인 화면으로 이동
+			return "redirect:/login/login?toURL="+request.getRequestURL();
+		
+		return "boardList";		//로그인 한 상태이면, 게시판 목록 화면으로 이동
+	}
+
+	private boolean loginCheck(HttpServletRequest request) {
+		//세션을 얻기
+		HttpSession session = request.getSession();
+		//세션에 id가 있는지 확인, 있으면 true가 반환
+		return session.getAttribute("id") != null;	//true 반환, null이면 false 반환	
+	}
+
+}
