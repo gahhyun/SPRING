ab47b5356a12e3d369baf707161603d997e42106
gahhyun <122864889+gahhyun@users.noreply.github.com>
2023-04-24 20:46:10 +0900

    오후 수업 추가

--- SpringDAOWeb/src/main/java/com/earth/work/RegisterController.java
+++ SpringDAOWeb/src/main/java/com/earth/work/RegisterController.java
@@ -27,19 +27,35 @@ public class RegisterController {
 	}
 	
 	@PostMapping("/register/save")
-	public String save(User user , Model m) throws UnsupportedEncodingException {
-		//유효성 검사  -> 따로 class만듦
+	public String save(@Valid User user, BindingResult result, Model m) throws UnsupportedEncodingException {
+		//유효성 검사  -> 관심사로 분리 , 따로 class만듦
 //		if(!isValid(user)) {	//isVaid는 ()개체에 대한 일부 유효성 검사를 수행하고 개체가 유효한지 여부를 나타내는 부울 값을 반환합니다.
 //			String msg = URLEncoder.encode("id를 잘못입력했습니다", "UTF-8");
 //			return "redirect:/register/add2?msg="+msg;			 //다시 입력받는 화면으로
 //		}
 		
+		System.out.println("user = " + user);
+		System.out.println("result = " + result);
+		
+		// User 객체 검증한 결과 에러가 있으면, 에러를 보여줌
 		//DB에 새회원 정보를 저장
-		int rowCnt = userDao.insertUser(user);
-		
-		if(rowCnt != FAIL) return "registerinfo";
-		
+		if (!result.hasErrors()) {
+			int rowCnt = userDao.insertUser(user);
+			if(rowCnt != FAIL) return "registerinfo";
+		}
 		return "registerForm";
+	}
+	
+	@InitBinder		// WebDataBinder에 검증기(Validator) 등록함
+	public void registerValidate(WebDataBinder binder) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+		//UserValidator를 WebDataBinder로 등록함 
+		binder.setValidator(new UserValidator());  
+		
+		//binder에 검증기가 등록되었는지 확인코드
+		List<Validator> validatorList = binder.getValidators();
+		System.out.println("validatorList = " + validatorList);
 	}
 
 	private boolean isValid(User user) {
