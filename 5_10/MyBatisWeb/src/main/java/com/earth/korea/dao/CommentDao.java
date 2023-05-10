a5cff521f573d900360f0c4cb77161840e579867
gahhyun <lgh002200273@gmail.com>
2023-05-10 15:57:35 +0900

    5/10 ajax연습, 댓글 mapper, dao, dto 만들기

---
+++ 5_10/MyBatisWeb/src/main/java/com/earth/korea/dao/CommentDao.java
@@ -0,0 +1,22 @@
+package com.earth.korea.dao;
+
+import java.util.List;
+
+import com.earth.korea.domain.CommentDTO;
+
+public interface CommentDao {
+	
+	int delete(Integer bno) throws Exception;
+	int delete(Integer cno, String commenter) throws Exception;
+	
+	List<CommentDTO> selectAll(Integer bno) throws Exception;
+	
+	int insert(CommentDTO commentDTO) throws Exception;
+	
+	int update(CommentDTO commentDTO) throws Exception;
+	
+	
+	
+	
+
+}
